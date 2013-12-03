db8084a60b930a169f6c2be8ab4b197428ad0435
Bennett Siegel <bennett.e.siegel@gmail.com>
2013-12-02 21:08:19 -0500

    fixed cancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,9 +2,6 @@ class Ability
   include CanCan::Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    if user.admin?
-      can :manage, :all
-    end
     if user.user?
      can :read, :all
      can :create, Home
